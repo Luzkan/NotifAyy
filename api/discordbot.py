41d237a8a289f32eead011a4545b128942aa1e8b
esavaner <44378917+esavaner@users.noreply.github.com>
2020-06-06 15:45:56 +0200

    multiple requests

--- api/discordbot.py
+++ api/discordbot.py
@@ -38,18 +38,16 @@ async def changes():
     global members
     while True:
         try:
-            response = requests.get(url + '/changes').json()
-            if response != []:
-                for r in response:
-                    for member in members:
-                        if member.id == int(r['discordId']) and r['change'] is not None:
-                            await member.create_dm()
-                            await member.dm_channel.send(
-                                'Recent changes: ' + r['change']
-                            )
+            for member in members:
+                response = requests.get(url + '/changes', params={'discordId': member.id})
+                if response is not None and response['change'] is not None:
+                    await member.create_dm()
+                    await member.dm_channel.send(
+                        'Recent changes: ' + response['change']
+                    )
 
-            else:
-                print('Empty response')
+                else:
+                    print('Empty response')
 
         except requests.exceptions.RequestException as err:
             print(err)
