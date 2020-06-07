abf54bc20626708595ce9a6c013f7046f24071d3
TeeKay18 <tommkar321@wp.pl>
2020-06-07 20:54:35 +0200

    Site monitor updated

--- msnotifier/bot/siteMonitor.py
+++ msnotifier/bot/siteMonitor.py
@@ -35,14 +35,55 @@ def split_content_by_tags(html: str, tags: List[str]) -> List[List[str]]:
     return [soup.find_all(tag) for tag in tags]
 
 
-def get_diffs(tags: List[str], addresses: List[str], t: float) -> list:
-    before = [split_content_by_tags(get_content(url), tags) for url in addresses]
+def get_diffs(user_id: int,
+              tags: List[str],
+              alert_ids: List[int],
+              addresses: List[str],
+              t: float) -> tuple:
+
+    before = [(alert_ids[i], split_content_by_tags(get_content(addresses[i]), tags))
+              for i in range(len(addresses))]
+
     time.sleep(t)
-    after = [split_content_by_tags(get_content(url), tags) for url in addresses]
-    comparison_lst = []
-    for i in range(len(before)):
-        comparison_lst.append(compare_content_by_tags(before[i], after[i]))
-    return comparison_lst
+
+    after = [(alert_ids[i], split_content_by_tags(get_content(addresses[i]), tags))
+             for i in range(len(addresses))]
+
+    comparison_lst = [(alert_ids[i], compare_content_by_tags(before[i][1], after[i][1]))
+                      for i in range(len(before))]
+
+    return user_id, comparison_lst
+
+
+def get_diffs_string_format(diffs: tuple, 
+                            titles: List[str], 
+                            addresses: List[str], 
+                            tags: List[str]) -> None:
+    message = ""
+    id_and_diffs = diffs[1]
+    for i in range(len(id_and_diffs)):
+        message += "\n"
+        message += ("Changes for alert number ")
+        message += str(id_and_diffs[i][0])
+        message += " with title: "
+        message += titles[i]
+        message += " and address: "
+        message += addresses[i]
+        message += "\n\n"
+        tag_diffs = id_and_diffs[i][1]
+        for h in range(len(tag_diffs)):
+            message += ("TAG")
+            message += tags[h]
+            message += "\n"
+            if tag_diffs[h]:
+                message += "\nBEFORE:\n"
+                message += str(tag_diffs[h][0][0])
+                message += "\nAFTER:\n"
+                message += str(tag_diffs[h][0][1])
+            else:
+                message += "No changes\n"
+    return message
+
 
 
 """
@@ -58,8 +99,8 @@ It means that mediamond.fi webpage didn't have any h1, h2 differences,
 while wp.pl had differences in h1, in that case there was one item with h1 tag, and afterwards None of them. 
 """
 if __name__ == "__main__":
-    time_seconds = 15
-    addresses = ["http://www.mediamond.fi", "https://wp.pl"]
-    for elem in get_diffs(["h1", "h2"], addresses, time_seconds):
-        for tag_diff in elem:
-            print(tag_diff)
+    time_seconds = 3
+    addresses = ["http://www.mediamond.fi", "https://wp.pl", "http://www.mediamond.fi"]
+    tags = ["h1", "h2", "h3", "p"]
+    x = get_diffs(14184148, tags, [1, 2, 3], addresses, time_seconds)
+    print(get_diffs_string_format(x, ["Alert 1", "Alert 2", "Alert 3"], addresses, tags))
